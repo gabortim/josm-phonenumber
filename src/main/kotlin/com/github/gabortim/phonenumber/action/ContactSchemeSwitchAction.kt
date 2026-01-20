2ca6c3c5873e97d58b15a7b0a99c19ee5c428a5c
Gábor Timár <23311361+gabortim@users.noreply.github.com>
2026-01-20 22:16:07 +0100

    Make Tags/Membership panel switched keys configurable, remove listener on destroy

--- src/main/kotlin/com/github/gabortim/phonenumber/action/ContactSchemeSwitchAction.kt
+++ src/main/kotlin/com/github/gabortim/phonenumber/action/ContactSchemeSwitchAction.kt
@@ -1,13 +1,16 @@
 package com.github.gabortim.phonenumber.action
 
 import com.github.gabortim.phonenumber.tool.NumberTools
+import com.github.gabortim.phonenumber.validation.PhoneNumberValidator.Companion.validatorPrefix
 import com.github.gabortim.phonenumber.validation.ValidatorConstants.CONTACT_SCHEME_PREFIX
 import com.github.gabortim.phonenumber.validation.ValidatorConstants.SEP
 import org.openstreetmap.josm.actions.JosmAction
 import org.openstreetmap.josm.command.ChangePropertyCommand
+import org.openstreetmap.josm.command.Command
 import org.openstreetmap.josm.command.SequenceCommand
 import org.openstreetmap.josm.data.UndoRedoHandler
 import org.openstreetmap.josm.data.osm.OsmPrimitive
+import org.openstreetmap.josm.data.preferences.ListProperty
 import org.openstreetmap.josm.gui.MainApplication
 import org.openstreetmap.josm.gui.layer.MainLayerManager
 import org.openstreetmap.josm.gui.layer.MainLayerManager.ActiveLayerChangeListener
@@ -27,12 +30,15 @@ class ContactSchemeSwitchAction :
     }
 
     companion object {
-        private val usableKeys = arrayOf("email", "phone", "mobile", "website", "facebook", "fax", "instagram", "youtube", "twitter", "linkedin")
+        private val usableKeys = ListProperty(
+            "$validatorPrefix.panelSwitchKeys",
+            mutableListOf("email", "phone", "mobile", "website", "facebook", "fax", "instagram", "youtube", "twitter", "linkedin")
+        )
     }
 
     override fun updateEnabledState(selection: Collection<OsmPrimitive>) {
         isEnabled = selection.any { primitive ->
-            primitive.hasKey(*usableKeys)
+            primitive.hasKey(*usableKeys.get().toTypedArray())
         }
     }
 
@@ -102,5 +108,9 @@ class ContactSchemeSwitchAction :
                 }
             })
         }
+    }
+
+    override fun destroy() {
+        MainApplication.getLayerManager().removeActiveLayerChangeListener(this)
     }
 }
